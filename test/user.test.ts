82fb5788b19daffec38d29ef79013984d5124ebe
GRIT-Tech-Inc <yada.yuki@fuji.waseda.jp>
2020-08-19 18:03:09 +0900

    add test

--- test/user.test.ts
+++ test/user.test.ts
@@ -7,10 +7,10 @@ describe("Test User Api",()=>{
     })
     it("Get Sample Api",async()=>{
         const response = await request(API_URL).get("/api/users/sample")
-        console.log(JSON.stringify(response))
         expect(response.status).toBe(200);
     })
     it("Post User For Registration",async()=>{
         const response = await request(API_URL).post("/api/users/regist").send({username:"yadayuki",email:"yada.yuki@fuji.waseda.jp",password:"password"})
+        expect(Object.keys(response.body)).toBe([["is_login","jwt"]])
     })
 })
